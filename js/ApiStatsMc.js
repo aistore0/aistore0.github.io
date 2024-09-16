b9e30b97aab19675adc719379f86b6e2d82b42b1
aistore <faisgaming131@gmail.com>
2024-09-16 16:06:06 +0900

    Add files via upload

---
+++ js/ApiStatsMc.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const serverIP = 'sg-1.ddns.net'; // Ganti dengan IP server Minecraft kamu
+    const Port = '19002'
+    const apiURL = `https://api.mcsrvstat.us/2/${serverIP}:${Port}`;
+
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+
+        const statusDiv = document.getElementById('server-status');
+        if (data.online) {
+            statusDiv.innerHTML = `
+              <img src="image/Brutalhard.png" alt="Logo" class="logo">
+                <h2>Server Info</h2>
+                <p>Server Description: ${data.motd.clean}</p>
+                <p>IP: ${serverIP}</p>
+                <p>Port: ${data.port}</p>
+                <p>Status: ONLINE</p>
+                <p>Players: ${data.players.online} / ${data.players.max}</p>
+                <p>Version: ${data.version}</p>
+                <a href="ticket.html" class="Button">Join Komunitas BHC</a>
+            `;
+        } else {
+            statusDiv.innerHTML = `
+                <h2>Server Info</h2>
+                <p>Status: OFFLINE</p>
+            `;
+        }
+    } catch (error) {
+        console.error('Error fetching server status:', error);
+        document.getElementById('server-status').innerHTML = 'Gagal memuat status server.';
+    }
+});
