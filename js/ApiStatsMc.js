6a3a7de96c70853d9e0ca642553eb54d387249da
aistore <faisgaming131@gmail.com>
2024-09-16 18:17:51 +0900

    Update ApiStatsMc.js

--- js/ApiStatsMc.js
+++ js/ApiStatsMc.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         const statusDiv = document.getElementById('server-status');
         if (data.online) {
             statusDiv.innerHTML = `
-              <img src="image/Brutalhard.png" alt="Logo" class="logo">
+              <img src="image/brutalhard.png" alt="Logo" class="logo">
                 <h2>Server Info</h2>
                 <p>Server Description: ${data.motd.clean}</p>
                 <p>IP: ${serverIP}</p>
